0b397caf38a44b419db10f0945d688c5aa89e0f5
Rohit Girdhar <rohit@iiith>
2014-03-31 10:19:23 +0530

    matlab batcher fixes

--- work/attempt2/utils/matlab_batcher.bash
+++ work/attempt2/utils/matlab_batcher.bash
@@ -1,10 +1,16 @@
 #!/bin/bash
+if [ $# -lt 1 ]
+then
+    echo 'Usage:' $0 'function_name arguments'
+    exit
+fi
 
-TMPDIR='tmp/'
+#TMPDIR='tmp/'
 matlab_exec=matlab
 X="${1}(${2})"
+TMP_FNAME=matlab_cmd_${1}.m
 mkdir -p $TMPDIR
-echo ${X} > $TMPDIR/matlab_command_${2}.m
-cat $TMPDIR/matlab_command_${2}.m
-${matlab_exec} -nojvm -nodisplay -nosplash < $TMPDIR/matlab_command_${2}.m
-rm $TMPDIR/matlab_command_${2}.m
+echo ${X} > $TMPDIR/$TMP_FNAME
+cat $TMPDIR/$TMP_FNAME
+${matlab_exec} -nojvm -nodisplay -nosplash < $TMPDIR/$TMP_FNAME
+rm $TMPDIR/$TMP_FNAME

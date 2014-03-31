30f37435d5f80bccae462373a74c4bdc6e1b8b74
Rohit Girdhar <rohit@iiith>
2014-03-31 20:40:21 +0530

    merging better

--- work/attempt2/homography/merge.py
+++ work/attempt2/homography/merge.py
@@ -8,13 +8,21 @@ def help():
     print 'Usage ./merge.py <source image path> <augmented image> <fovy scale ratio>'
 
 def merge(overlay, orig):
-    res = []
-    for i in range(orig.shape[0]):
-        for j in range(orig.shape[1]):
-            if np.linalg.norm(overlay[i][j]) > 5:
-                orig[i][j] = overlay[i][j]
+    rows,cols,channels = overlay.shape
+    roi = orig[0:rows, 0:cols]
+
+    tmp = cv2.cvtColor(overlay, cv2.COLOR_BGR2GRAY);
+    retval, mask = cv2.threshold(tmp,10,255, cv2.THRESH_BINARY);
+    mask_inv = cv2.bitwise_not(mask)
+
+    orig_bg = cv2.bitwise_and(roi,roi,mask = mask_inv)
+    overlay_fg = cv2.bitwise_and(overlay,overlay,mask = mask)
+
+    dst = cv2.add(orig_bg,overlay_fg)
+    orig[0:rows, 0:cols ] = dst
+
     return orig
-    
+
 if len(sys.argv) < 4:
     help()
     sys.exit(0)

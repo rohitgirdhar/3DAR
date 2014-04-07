64437fe2a45e5958901889081930f7f5be63a9cb
Rohit Girdhar <rohit@iiith>
2014-04-07 09:49:22 +0530

    new merge for recalib

---
+++ work/attempt2/recalib2/merge.py
@@ -0,0 +1,1 @@
+../homography/merge.py
--- work/attempt2/recalib2/run.sh
+++ work/attempt2/recalib2/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ $# -lt 6 ]
+if [ $# -lt 5 ]
 then
-    echo 'Use: ' $0 ' <IMGS_DIR_PATH> <imgID_toAug> <imgID2_closestMatch> <ptcorr_dir_path> <3D model path> <result_img_path>'
-    echo 'eg ./prog ../Images_bob 00000000 00000001 ../../BobReconst/ptcorrs/ ../../BobReconst/3DModels/cap_aligned.obj res.jpg'
+    echo 'Use: ' $0 ' <IMGS_DIR_PATH> <imgID_toAug> <imgID2_closestMatch> <ptcorr_dir_path> <3D model path>'
+    echo 'eg ./prog ../Images_bob 00000000 00000001 ../../BobReconst/ptcorrs/ ../../BobReconst/3DModels/cap_aligned.obj'
     exit
 fi
 
 bash runCalib.sh $1 $2 $3 $4
 render/render cam.txt 1 snap.jpg $5
-composite -blend 60 snap.jpg ${1}/${2}.jpg ${6}
+python merge.py ${1}/${2}.jpg snap.jpg 1

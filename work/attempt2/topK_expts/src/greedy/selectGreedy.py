5bad8d0596db8d032eb54a5f8aec8a2cb6ce4fcb
Rohit Girdhar <rohit@iiith>
2014-05-08 17:54:50 +0530

    calc error and output to file

--- work/attempt2/topK_expts/src/greedy/selectGreedy.py
+++ work/attempt2/topK_expts/src/greedy/selectGreedy.py
@@ -34,12 +34,13 @@ def selectGreedily(E, K, useMax):
                 err[0][i] = E[maxdiff_i][i]
                 match[0][i] = maxdiff_i
         K -= 1
-    return selected,match
+    return selected,match,sum(err[0][:])
 
 def main():
 
     parser = argparse.ArgumentParser(description='Greedy aprox to top-K optimization')
-    parser.add_argument('-f', '--fname', nargs=1, required=True, help='Error Filename')
+    parser.add_argument('-f', '--fname', nargs=1, required=True, help='Input Error Filename')
+    parser.add_argument('-o', '--output-fname', nargs=1, required=True, help='Output Filename')
     parser.add_argument('-K', nargs=1, type=int, required=True, help='Size of K set')
     parser.add_argument('--max', action='store_const', const=True, help='Use the max instead of sum')
 
@@ -47,15 +48,19 @@ def main():
     
     error_file = args.fname[0]
     K = args.K[0]
+    output_fname = args.output_fname[0]
 
     E = np.genfromtxt(error_file, dtype=float, delimiter=' ')
-    sel,match = selectGreedily(E, K, args.max)
+    sel,match,tot_err = selectGreedily(E, K, args.max)
+    f = open(output_fname, 'w')
     for i in range(np.shape(E)[0]):
         if sel[0][i]:
-            print 's'
+            print >> f, 's'
         else:
-            print 'h',
-            print int(match[0][i]) + 1 # to make it 1 indexed
+            print >> f, 'h',
+            print >> f, int(match[0][i]) + 1 # to make it 1 indexed
+    f.close()
+    print 'Total Error', tot_err
 
 if __name__ == '__main__':
     main()
